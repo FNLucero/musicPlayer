c84ea38bb146df796eb97e9d3c665a1cd71d2611
FNLucero <crossgotica@gmail.com>
2021-11-12 16:01:48 -0300

    reproducción

--- MyApp/AudioPlayerViewController.swift
+++ MyApp/AudioPlayerViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import SwiftySound
 
 class AudioPlayerViewController: UIViewController {
 
-    var isPlaying: Bool = false
+    var isPlaying: Bool = true
     var songTitle: String?
     let labelTitulo = UILabel()
+    let sliderTrack = UISlider()
+    let sliderVolume = UISlider()
+    var song: Sound?
     
-    
+    var imageViewGif: UIImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,15 +39,16 @@ class AudioPlayerViewController: UIViewController {
         buttonPlay.translatesAutoresizingMaskIntoConstraints = true
         buttonPlay.frame = CGRect(x: 20, y: 100, width: 100, height: 40)
         self.view.addSubview(buttonPlay)
+        buttonPlay.addTarget(self, action: #selector(botonTouchPlay), for: .touchUpInside)
         
         let buttonStop = UIButton(type: .system)
         buttonStop.setTitle("Stop", for: .normal)
         buttonStop.autoresizingMask = .flexibleWidth
         buttonStop.translatesAutoresizingMaskIntoConstraints = true
-        buttonStop.frame = CGRect(x: 250, y: 100, width: 100, height: 40)
+        buttonStop.frame = CGRect(x: self.view.frame.width-120, y: 100, width: 100, height: 40)
         self.view.addSubview(buttonStop)
+        buttonStop.addTarget(self, action: #selector(botonTouchPause), for: .touchUpInside)
         
-        let sliderTrack = UISlider()
         sliderTrack.autoresizingMask = .flexibleWidth
         sliderTrack.translatesAutoresizingMaskIntoConstraints = true
         sliderTrack.frame = CGRect(x: 20, y: 150, width: self.view.frame.width-40, height: 50)
@@ -58,51 +63,79 @@ class AudioPlayerViewController: UIViewController {
         labelVolumen.textAlignment = .center
         self.view.addSubview(labelVolumen)
         
-        let sliderVolume = UISlider()
         sliderVolume.autoresizingMask = .flexibleWidth
         sliderVolume.translatesAutoresizingMaskIntoConstraints = true
         sliderVolume.frame = CGRect(x: 20, y: 250, width: self.view.frame.width - 40, height: 30)
+        sliderVolume.value = 1
         self.view.addSubview(sliderVolume)
         
-        /*
-        var imageViewGif = UIImageView()
-        imageViewGif.translatesAutoresizingMaskIntoConstraints = true
-        imageViewGif.frame = CGRect(x: 0, y: 4, width: 35, height: 35)
-        imageViewGif.backgroundColor = UIColor.white
-        imageViewGif.image = UIImage(named: "ImageAlbum")
-        self.view.addSubview(imageViewGif)*/
-        
-        /*
-        if let fileURL = Bundle.main.url(forResource: "stegosaurus-studio", withExtension: ".gif") {
-            let gifImage = UIImage.animatedImage(withAnimatedGIFURL: fileURL)
-            let imageViewGif = UIImageView(image: gifImage)
-            imageViewGif.autoresizingMask = .flexibleWidth
-            imageViewGif.translatesAutoresizingMaskIntoConstraints=true
-            imageViewGif.frame=CGRect(x: 0, y: 350, width: self.view.frame.width, height: 150)
-            self.view.addSubview(imageViewGif)
-        }*/
-        
-        guard let fileURL = Bundle.main.url(forResource: "stegosaurus-studio", withExtension: ".gif") else{
+        enableGif()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard let fileSongURL = Bundle.main.url(forResource: "bensound-ukulele", withExtension: ".mp3") else{
             return
         }
-        let gifImage = UIImage.animatedImage(withAnimatedGIFURL: fileURL)
-        let imageViewGif = UIImageView(image: gifImage)
-        imageViewGif.autoresizingMask = .flexibleWidth
-        imageViewGif.translatesAutoresizingMaskIntoConstraints=true
-        imageViewGif.frame=CGRect(x: 0, y: 300, width: self.view.frame.width, height: 150)
-        self.view.addSubview(imageViewGif)
+        Sound.enabled = true
+        song = Sound(url: fileSongURL)
+        song?.volume = sliderVolume.value
+        song?.play()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            print("Landscape")
+            imageViewGif!.frame=CGRect(x: 0, y: 300, width: self.view.frame.width, height: 150)
+        } else {
+            print("Portrait")
+            imageViewGif!.frame=CGRect(x: 0, y: 300, width: self.view.frame.width, height: 150)
+        }
     }
     
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         print(#function)
         self.isPlaying = !(self.isPlaying)
         print("la variable is Playing esta en \(isPlaying)")
+        if self.isPlaying {
+            enableGif()
+            song?.resume()
+        }
+        else {
+            disableGif()
+            song?.pause()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         labelTitulo.text = songTitle
     }
     
+    func enableGif(){
+        guard let fileGIFURL = Bundle.main.url(forResource: "stegosaurus-studio", withExtension: ".gif") else{
+            return
+        }
+        let gifImage = UIImage.animatedImage(withAnimatedGIFURL: fileGIFURL)
+        imageViewGif = UIImageView(image: gifImage)
+        imageViewGif!.autoresizingMask = .flexibleWidth
+        imageViewGif!.translatesAutoresizingMaskIntoConstraints=true
+        imageViewGif!.frame=CGRect(x: 0, y: 300, width: self.view.frame.width, height: 150)
+        self.view.addSubview(imageViewGif!)
+    }
+    
+    func disableGif(){
+        imageViewGif!.image = UIImage(named: "stopedGif")
+    }
+    
+    @objc func botonTouchPlay(){
+        enableGif()
+        song?.resume()
+    }
+    
+    @objc func botonTouchPause(){
+        disableGif()
+        song?.pause()
+    }
     
 
     /*
